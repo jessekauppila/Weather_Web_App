eae218c241b83eb355b77aba93720fd2ff3debca
jessekauppila <jesse.kauppila@gmail.com>
2024-11-22 14:47:59 -0800

    Was able make branch of hourly error filtered data and connect that to day data

--- app/filteredObservationData.ts
+++ app/filteredObservationData.ts
@@ -10,14 +10,14 @@ export function filteredObservationData(
 ) {
   const { startHour, endHour, mode } = options;
 
-  console.log('observationsData in filteredObservationData:', observationsData);
+  //console.log('observationsData in filteredObservationData:', observationsData);
 
   // Group observations
   const groupedObservations = mode === 'summary' 
     ? groupByStation(observationsData)
     : groupByDay(observationsData, startHour, endHour);
 
-  console.log('groupedObservations in filteredObservationData:', groupedObservations);
+  //console.log('groupedObservations in filteredObservationData:', groupedObservations);
 
   // Filter and process observations
   const filteredGroupedObservations = Object.entries(groupedObservations).reduce((acc, [key, observations]) => {
@@ -38,7 +38,7 @@ export function filteredObservationData(
         SNOW_DEPTH_24H_CONFIG
       );
 
-    const filteredObservations = observations.map((obs, index) => ({
+    const filteredObservations = observations.map((obs: Record<string, any>, index: number) => ({
       ...obs,
       snow_depth: filteredSnowDepth[index]?.snow_depth,
       snow_depth_24h: filteredSnowDepth24h[index]?.snow_depth
@@ -48,64 +48,9 @@ export function filteredObservationData(
     return acc;
   }, {} as typeof groupedObservations);
 
-  // Process the filtered data
-  return Object.entries(filteredGroupedObservations).map(([stid, stationObs]) => {
-    // Process measurements and create averages object
-    const averages: { [key: string]: number | string | any[] } = {
-      Stid: stid,
-      Station: stationObs[0].station_name,
-      Latitude: Number(stationObs[0].latitude),
-      Longitude: Number(stationObs[0].longitude),
-      Elevation: `${Number(stationObs[0].elevation)} ft`,
-      observations: stationObs
-    };
-
-    // Process each measurement type
-    const measurementKeys = [
-      'air_temp',
-      'precip_accum_one_hour',
-      'relative_humidity',
-      'snow_depth',
-      'snow_depth_24h',
-      'wind_speed',
-      'wind_gust',
-      'wind_direction',
-    ];
-
-    measurementKeys.forEach((key) => {
-      const values = stationObs
-        .map((obs: Record<string, any>) => obs[key])
-        .filter((val: any): val is number | string => val !== null);
-      if (values.length > 0) {
-        averages[key] = values;
-      }
-    });
-
-    // Special processing for certain fields
-    if (
-      Array.isArray(averages['wind_speed']) &&
-      averages['wind_speed'].every((v) => v === '')
-    ) {
-      averages['wind_speed'] = [''];
-    }
-
-    ['intermittent_snow', 'precipitation'].forEach((key) => {
-      averages[key] = [stationObs[0][key] || ''];
-    });
-
-    // Process date_time
-    averages['date_time'] = stationObs.map(
-      (obs: Record<string, any>) => obs.date_time
-    );
-
-    return averages;
-  });
-
-  //console.log('filteredGroupedObservations in filteredObservationData:', filteredGroupedObservations);  
-  //return filteredGroupedObservations;
-
+  //console.log('filteredGroupedObservations in filteredObservationData:', filteredGroupedObservations);
+  return filteredGroupedObservations;
 }
-
 
 // Helper functions
 // Helper function to group by station (current behavior)
