65e9fb3f5b6a983eadb9a958617c3d80cd6fe472
jessekauppila <jesse.kauppila@gmail.com>
2025-02-02 15:24:26 -0800

    Whitetail Pass and Alpental Summit added to zones

--- app/config/regions.ts
+++ app/config/regions.ts
@@ -2,13 +2,13 @@
 export const stationGroups = {
     westSlopesNorth: ['5', '6'],
     westSlopesCentral: ['57'],
-    westSlopesSouth: ['29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '54'],
+    westSlopesSouth: ['29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '54','49'],
     eastSlopesNorth: ['7', '8', '9'],
     eastSlopesCentral: ['11', '24', '25', '26', '19','48'],
     eastSlopesSouth: [],
     olympics: ['4'],
     mtHood: ['41', '42', '43', '44', '45', '46', '47', '56'],
-    snoqualmie: ['1', '2', '20', '21', '22', '23'],
+    snoqualmie: ['1', '2', '3','20', '21', '22', '23'],
     stevensPass: ['13', '14', '17', '18', '50', '51'],
   };
   
