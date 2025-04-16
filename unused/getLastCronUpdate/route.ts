d26bcba7960932489f9cb283e6e5c9499461a3ec
jessekauppila <jesse.kauppila@gmail.com>
2025-04-16 11:53:32 -0700

    Deleted folder of unused components

--- unused/getLastCronUpdate/route.ts
+++
@@ -1,7 +0,0 @@
-import { kv } from '@vercel/kv';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const lastUpdate = await kv.get('lastCronUpdate');
-  return NextResponse.json({ lastUpdate });
-}
