28bc767b1e28df1fc8264d490c19f17d7b65875b
jessekauppila <jesse.kauppila@gmail.com>
2025-06-30 09:27:34 -0700

    Snow depth, max, min temp, and snow accum on top

--- app/map/layers/temperatureWithColumns/maxTempLayer.tsx
+++ app/map/layers/temperatureWithColumns/maxTempLayer.tsx
@@ -14,7 +14,9 @@ export function createMaxTempLayer(
   return new IconLayer({
     id: 'maxTempIcons',
     data: data.features,
+
     billboard: false,
+
     autoHighlight: true,
     getIcon: (f) => {
       if (!f?.properties?.airTempMax) {
@@ -37,7 +39,7 @@ export function createMaxTempLayer(
     ],
     getSize: 100,
     getAngle: 0,
-    angleAlignment: 'viewport',
+    // angleAlignment: 'viewport',
     iconAtlas: '/maxTempAtlas/maxTemp_location_icon_atlas.png',
     iconMapping: '/maxTempAtlas/location-icon-mapping.json',
     pickable: true,
@@ -45,6 +47,12 @@ export function createMaxTempLayer(
     shadowEnabled: false,
     alphaCutoff: 0.05,
     sizeScale: 1,
+
+    angleAlignment: 'viewport', //'viewport' for 3d, 'screen' for 2d
+    parameters: {
+      depthTest: false,      // Disable depth testing completely
+      depthMask: false
+    },
     extensions: [new TerrainExtension()],
   });
 } 
