048775105aff1fa4597df0ef76b38e046d37158b
jessekauppila <jesse.kauppila@gmail.com>
2025-06-30 09:15:41 -0700

    Liquid precip on top

--- app/map/layers/snow-accum/boundaryLayer.tsx
+++ app/map/layers/snow-accum/boundaryLayer.tsx
@@ -1,6 +1,7 @@
 import { IconLayer } from '@deck.gl/layers';
 import type { Feature, Geometry } from 'geojson';
 import { Map_BlockProperties } from '../../map';
+import { _TerrainExtension as TerrainExtension } from '@deck.gl/extensions';
 
 export function createSnowDepthBoundaryLayer(
   data: {
@@ -21,12 +22,17 @@ export function createSnowDepthBoundaryLayer(
     getElevation: () => 5000, 
     getSize: 100,
     getAngle: 0,
-    angleAlignment: 'viewport',
     iconAtlas: '/snowDepthAtlasRedo/snowDepth_boundary_atlas.png',  // Make sure this file exists
     iconMapping: '/snowDepthAtlasRedo/boundary-icon-mapping.json',  // This path is correct
     pickable: false,
     shadowEnabled: false,
     alphaCutoff: 0.05,
     sizeScale: 1,
+    angleAlignment: 'viewport', //'viewport' for 3d, 'screen' for 2d
+    parameters: {
+      depthTest: false,      // Disable depth testing completely
+      depthMask: false
+    },
+    extensions: [new TerrainExtension()],
   });
 } 
